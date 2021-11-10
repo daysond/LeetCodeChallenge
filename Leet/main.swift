ecc239986432bdb633e030c219e306bc29cb39ed
daysond <daysond@outlook.com>
2021-11-09 22:18:53 -0500

    TwoSum Success

--- Leet/main.swift
+++ Leet/main.swift
@@ -7,3 +7,22 @@
 
 import Foundation
 
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        
+        for (index, lhs) in nums.enumerated() {
+            
+            for n in index+1...nums.count-1 {
+                if lhs + nums[n] == target {
+                    return [index, n]
+                }
+            }
+        }
+        return [0,0]
+    }
+}
+
+/*
+ Runtime: 72 ms, faster than 51.84% of Swift online submissions for Two Sum.
+ Memory Usage: 14.4 MB, less than 52.49% of Swift online submissions for Two Sum.
+ */
