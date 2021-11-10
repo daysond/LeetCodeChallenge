83134253925fd73b248a04f5901a7a38f1d3c8c5
daysond <daysond@outlook.com>
2021-11-09 22:40:35 -0500

    TwoSum with two solutions

--- Leet/main.swift
+++ Leet/main.swift
@@ -7,22 +7,43 @@
 
 import Foundation
 
-class Solution {
-    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+
+func twoSumA(_ nums: [Int], _ target: Int) -> [Int] {
+    
+    for (index, lhs) in nums.enumerated() {
         
-        for (index, lhs) in nums.enumerated() {
-            
-            for n in index+1...nums.count-1 {
-                if lhs + nums[n] == target {
-                    return [index, n]
-                }
+        for n in index+1...nums.count-1 {
+            if lhs + nums[n] == target {
+                return [index, n]
             }
         }
-        return [0,0]
     }
+    return [0,0]
 }
+
 
 /*
  Runtime: 72 ms, faster than 51.84% of Swift online submissions for Two Sum.
  Memory Usage: 14.4 MB, less than 52.49% of Swift online submissions for Two Sum.
  */
+
+func twoSumB(_ nums: [Int], _ target: Int) -> [Int] {
+    
+    var numMap = [Int:Int]()
+    var rhs = 0
+    for (index, lhs) in nums.enumerated() {
+        rhs = target - lhs;
+        if  numMap[rhs] != nil {
+            return [numMap[rhs]!, index]
+        }
+        numMap[lhs] = index
+    }
+    return [0,0]
+}
+
+/*
+ Runtime: 44 ms, faster than 99.73% of Swift online submissions for Two Sum.
+ Memory Usage: 14.3 MB, less than 72.79% of Swift online submissions for Two Sum.
+ */
+
+
